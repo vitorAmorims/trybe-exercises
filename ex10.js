cd376d8a7fb2cb4c9de2a1642d786151f962e05c
Vitor Amorims <vtamorims@gmail.com>
2020-08-28 07:15:15 -0300

    Ex10 Descrevendo como realizei operaçao de lucro dos produtos.

--- ex10.js
+++ ex10.js
@@ -12,14 +12,14 @@ lucro = valorVenda - valorCustoTotal (lucro de um produto)
 //inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda.
 let produtoA = new Array;
 let produtoB = new Array;
-produtoA.push(100,200);
+
+produtoA.push(100,200); //produtosA[0] possui o custo e produtosA[1] possui o preço de venda
 produtoB.push(55,150);
-impostoA = ((produtoA[0] / 100)*20)
+
+impostoA = ((produtoA[0] / 100)*20) //sob os produtos incide 20% de imposto.
 impostoB = ((produtoB[0] / 100)*20)
-
-lucroA = produtoA[1] - (produtoA[0]+impostoA);
+lucroA = produtoA[1] - (produtoA[0]+impostoA); //lucro = preço de venda - (preço de custo + imposto)
 lucroB = produtoB[1] - (produtoB[0]+impostoB);
-
 console.log(`Ao vender 1000 peças do produto A haverá lucro de ${lucroA*1000}`);
 console.log(`Ao vender 1000 peças do produto B haverá lucro de ${lucroB*1000}`);
 
