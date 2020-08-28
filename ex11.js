630475d9a5392fece2817ce563a72f818393d34a
Vitor Amorims <vtamorims@gmail.com>
2020-08-28 10:10:56 -0300

    Resoluçao do ex11 inserido table de imposto de renda e concluida funçao de calculo salario liquido.

--- ex11.js
+++ ex11.js
@@ -25,27 +25,6 @@ De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzi
 
 Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
 
-Exemplo: Uma pessoa possui o salário bruto de R$ 3.000,00. O cálculo será:
-
-O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
-
-Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
-
-Para pegar o valor do IR, temos um salário (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, sendo a alíquota, então, de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. Assim, temos:
-
-R$ 2.670,00: salário com INSS já deduzido;
-
-7.5%: alíquota de imposto de renda;
-
-R$ 142,00 parcela a se deduzir do imposto.
-
-Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
-
-O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
-
-Resultado: R$ 2.612,55.
-
-Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?
 */
 let salario_bruto;
 let salario_base;
@@ -79,25 +58,32 @@ function calcularSalarioLiq (param){
         inss = 0;
         salario_base = 0
     }
-    if (salario_base <= 1903,98){
-        aliquota_ir = 0;
-    }else if (salario_base >= 1903,99 && salario_base <= 2826,65){
+    /*
+    Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
+
+O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
+*/
+    if (salario_base >= 1903,99 && salario_base <= 2826,65){
         aliquota_ir = 7.5;
-        ir = (salario_base /100) * aliquota_ir;
         parcela_reduzir = 142,80; 
+        ir = (((salario_base /100) * aliquota_ir) - parcela_reduzir);
+        salario_liq = salario_base - ir;
     }else if(salario_base >= 2826,66 && salario_base <= 3751,05){
         aliquota_ir = 15;
-        ir = (salario_base /100) * aliquota_ir;
         parcela_reduzir = 354,80;
+        ir = (((salario_base /100) * aliquota_ir) - parcela_reduzir);
+        salario_liq = salario_base - ir;
     }else if (salario_base >= 3751,06 && salario_base <= 4664,68){
         aliquota_ir = 22.5;
-        ir = (salario_base /100) * aliquota_ir;
         parcela_reduzir = 636,13;
-    }else if (salario_base > 4664,68){
+        ir = (((salario_base /100) * aliquota_ir) - parcela_reduzir);
+        salario_liq = salario_base - ir;
+    }else if(salario_base > 4664,68){
         aliquota_ir = 27.5;
-        ir = (salario_base /100) * aliquota_ir;
         parcela_reduzir = 869,36;
+        ir = (((salario_base /100) * aliquota_ir) - parcela_reduzir);
+        salario_liq = salario_base - ir;
     }
-    console.log(salario_base);//inss aliquotas ok
+    console.log(salario_liq);
 }
 calcularSalarioLiq(3100);
