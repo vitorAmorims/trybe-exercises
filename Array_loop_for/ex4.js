dd97f84e18d287e3411000f103eaaeaf1563835a
Vitor Amorims <vtamorims@gmail.com>
2020-08-28 10:59:12 -0300

    Resoluçao do ex4 com loop for e estrutura if para variavel media.

--- Array_loop_for/ex4.js
+++ Array_loop_for/ex4.js
@@ -1,5 +1,5 @@
 /*
-Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+Com o mesmo código do exercício anterior, caso valor final da media seja maior que 20, imprima a mensagem: "valor de média maior que 20". Caso não seja, imprima a mensagem: "valor de média menor ou igual a 20";
 */
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let total = 0;
@@ -7,6 +7,11 @@ let media = 0;
 for (let i = 0; i < numbers.length; i++){
     total += (numbers[i]);
     media = total / numbers.length;
+}
+if (media > 20){
+    console.log("Valor de média maior que 20.");
+}else{
+    console.log("valor de mpedia menor ou igual a 20.");
 }
 console.log(`O vetor possui ${numbers.length} elementos.`);
 console.log(`A soma dos elementos no vetor é ${total}`);
