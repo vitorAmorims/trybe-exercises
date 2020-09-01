7970932c5913e345e3143386ffb5b9a682e90805
Vitor Amorims <vtamorims@gmail.com>
2020-09-01 11:58:32 -0300

    For in loop para iterar objeto.

---
+++ exw3s.js
@@ -0,0 +1,6 @@
+//Crie um loop que percorre cada item da fruitsmatriz.
+var frutas = ["Maçã", "Banana", "Laranja"];
+for (let i in frutas) {
+    console.log(i); // percorre o indice do array
+    console.log(frutas[i]); //percorre o valor do respectivo indice.
+}
