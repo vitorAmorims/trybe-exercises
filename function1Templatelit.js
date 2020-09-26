d5bcdb3cc84da42b1fe3241745aa43eb7430a434
vitorAmorims <vtamorims@gmail.com>
2020-09-26 12:36:52 -0300

    resoluçao das funçoes template literals

--- function1Templatelit.js
+++ function1Templatelit.js
@@ -1,7 +1,7 @@
+let frase1 = "Tryber x aqui!"
 function templateLiteral (param) {
-    let frase = "Tryber x aqui!";
-    frase = frase.replace("x", param);
-    return console.log(frase);
+    frase1 = frase1.replace("x", param);
+    return console.log(frase1);
 }
 templateLiteral("bebeto");
 
@@ -11,5 +11,25 @@ const templateLiteral1 = (param) => {
     frase = frase.replace("x", param);
     return console.log(frase);
 }
-templateLiteral("ensinando");
+templateLiteral1("ensinando");
 
+let skills = ["determinação", "auto-didata", "html", "css", "javascript"];
+
+function fskills (param) {
+    for (let i = 0; i < skills.length -1; i++) {
+        for (let j = 0; j < skills.length; j++) {
+            let x;
+            if (skills[i] < skills[j]) {
+                x = skills[i];
+                skills[i] = skills[j];
+                skills[j] = x;
+            }
+        }
+    }
+    console.log(`${frase1} Minhas cinco principais habilidades são:`)
+    for (let i = 0; i < skills.length; i++) {
+        console.log(`\n ${i}-${skills[i]}`);
+    }
+        
+}
+fskills(frase1);
