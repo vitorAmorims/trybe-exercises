a89a3eb6f331b1897544c4443de4acd4e9ea26e0
vitorAmorims <vtamorims@gmail.com>
2020-09-28 11:41:26 -0300

    ex2 parte2 funçao para listar as chaves de um objeto.

--- exercises/two/ex1.js
+++ exercises/two/ex1.js
@@ -29,3 +29,14 @@ const lesson1 = {
  }
 adicionarTurnoManha(lesson2, "turno", "manhã");
 console.log(lesson2);
+
+/*
+Crie uma função para listar as keys de um objeto. 
+Essa função deve receber um objeto como parâmetro.
+*/
+
+function listarKeys (objeto) {
+    let keys = Object.keys(objeto)
+    console.log(keys)
+}
+listarKeys(lesson2)
