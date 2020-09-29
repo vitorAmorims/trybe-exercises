bec558a6de8534b44e20902f1ef72f4d0b804637
vitorAmorims <vtamorims@gmail.com>
2020-09-29 10:00:42 -0300

    Resposta do ex3 questao 2 com metodo notDeepStrictEqual analisando entrada dos parametros e respectivo retorno

--- ex3.js
+++ ex3.js
@@ -17,3 +17,6 @@ assert.strictEqual(typeof myRemoveWithoutCopy, 'function');
 
 //1 - Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
 assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1,2,4], "mensagem de erro para o exerício 1");
+
+//2 - Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1,2, 3, 4], "Mensagem de erro para questão 2");
