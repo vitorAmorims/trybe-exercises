7dd81134f2ad6b7e329c9c46a3c4b7ebb301e926
vitorAmorims <vtamorims@gmail.com>
2020-09-29 09:46:08 -0300

    Resoluçao do ex2 questao 3, verificando se houve alteraçao no array que serve de param para funçao myRemove

--- ex2.js
+++ ex2.js
@@ -14,10 +14,17 @@ function myRemove(arr, item) {
 assert.strictEqual(typeof myRemove, 'function');
 
 //1 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
-assert.deepStrictEqual(myRemove([1,2,3,4], 3), [1,2,4]);
+//assert.deepStrictEqual(actual, expected[, message])
+assert.deepStrictEqual(myRemove([1,2,3,4], 3), [1,2,4], "mensagem ex1");
 
 //2 - Verifique se a chamada myRemove([1, 2, 3, 4], 1) não retorna o array [1, 2, 3, 4]
-assert.notDeepStrictEqual(myRemove([1,2,3,4],3),[1, 2, 3, 4],"mensagem de erro");
+assert.notDeepStrictEqual(myRemove([1,2,3,4],3),[1, 2, 3, 4],"mensagem de erro ex 2");
+
+//3- Verifique se o array passado por parâmetro não sofreu alterações
+let array = [1, 2, 3, 4];
+//detalhe o ex. pede array passado por parâmetro não alterações;
+assert.deepStrictEqual(myRemove(array, [1, 2, 3, 4]), [1, 2, 3, 4], "Mensagem de erro do ex 3");
+
 
 /* teste com método assert*/
 // var x = { a : { n: 0 } };
