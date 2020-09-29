8c2625bb9fd9e9affc7ae2ac524935c2bec76ac9
vitorAmorims <vtamorims@gmail.com>
2020-09-29 19:05:09 -0300

    require('assert'), alteraçao na funçao e retorno de mensagem com variaveis concatenadas.

--- subscribe_functions_tdd/ex1.js
+++ subscribe_functions_tdd/ex1.js
@@ -1,13 +1,17 @@
+const assert = require('assert');
 //Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 
 const greetPeople = (people) => {
-    let greeting = 'Hello ';
+  let greeting = [];
+  let msg = 'Hello ';
   
-    for (const person in people) {
-      greeting += people[person];
-    }
-    return greeting;
-  };
+  for (const person in people) {
+    greeting.push(msg + people[person]);
+  }
+  return greeting;
+};
   
   const parameter = ['Irina', 'Ashleigh', 'Elsa'];
   const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+console.table(greetPeople(parameter)); //1 - executei a func para ver o seu retorno
+assert.deepStrictEqual(greetPeople(parameter), result);
