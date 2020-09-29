d77a9e6e120ce298fb388227f6e28356e9fd860a
vitorAmorims <vtamorims@gmail.com>
2020-09-29 15:08:33 -0300

    funçao para iterar array e trazer posiçao se o param estiver contido no array

---
+++ tdd/ex4.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+function findTheNeedle (array, param) {
+    let output;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === param) {
+           output = i;
+        } //else {
+           //output = -1;
+        //} 
+    }
+    return output;
+}
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+assert.throws(findTheNeedle, 'function');
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+// words = ['plant', 'shelf', 'arrow', 'bird'];
+// expected = 0;
+// output = findTheNeedle(words, 'plant');
+// assert.strictEqual(output, expected);
+
+// words = ['plant', 'shelf', 'arrow', 'bird'];
+// expected = -1;
+// output = findTheNeedle(words, 'plat');
+// assert.strictEqual(output, expected);
