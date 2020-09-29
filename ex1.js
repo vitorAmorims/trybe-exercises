1b2b1f123ec0aca45551d76f7b44e57105cd383a
vitorAmorims <vtamorims@gmail.com>
2020-09-28 21:26:25 -0300

    Resoluçao dos exercicios parte 1

--- ex1.js
+++ ex1.js
@@ -8,7 +8,18 @@ function sum(a, b) {
   return a + b;
 }
 
-// implemente seus testes aqui
+//implemente seus testes aqui
 assert.strictEqual(typeof sum, 'function');
+
 //Teste se o retorno de sum(4, 5) é 9
 assert.strictEqual(sum(4, 5), 9);
+
+//Teste se o retorno de sum(0, 0) é 0
+assert.strictEqual(sum(0, 0), 0);
+
+//Teste se a função sum lança um erro quando os parâmetros são 4 e "5"(string 5)
+assert.throws(() => {
+    sum(4, '5')
+});
+
+// sum(4,'5');
