f98cef74c983ac5845749a51fd93d5a1589bf655
vitorAmorims <vtamorims@gmail.com>
2020-09-29 10:10:38 -0300

    Inicio do ex4

---
+++ ex4.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(typeof myFizzBuzz, 'function');
