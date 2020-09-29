f78e49d95c29771f37d8d4afcbf470e7357579c8
vitorAmorims <vtamorims@gmail.com>
2020-09-29 11:10:46 -0300

    comparando objeto 2 objeto 3, com retorno esperaddo do metodo deepStrictEqual

--- ex5.js
+++ ex5.js
@@ -18,9 +18,12 @@ const obj3 = {
 // implemente seus testes aqui
 //1 - Compare dois objetos (JSON) para verificar se são idênticos ou não
 //assert.deepStrictEqual(actual, expected[, message])
+//verificando objetos 1 e 2
 assert.deepStrictEqual(obj1,obj2, "Objetos com keys diferentes!");
 
 //2- verificando objetos 1 e 3
-assert.deepStrictEqual(obj1, obj3, "Objetos com keys diferentes! obj1 e obj3")
+// assert.deepStrictEqual(obj1, obj3, "Objetos com keys diferentes! obj1 e obj3")
 
-//3 - 
+//3 - verificando objettos 2 e 3
+assert.deepStrictEqual(obj2, obj3, "Objetos com keys diferentes! obj2 e obj3");
+
