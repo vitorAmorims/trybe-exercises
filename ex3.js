5d9e692aec16f74342739b6122135ce072a3a80c
vitorAmorims <vtamorims@gmail.com>
2020-09-29 09:57:45 -0300

    resposta do ex3 questao1 metodo deepStricEqual analisando o param de entrada da funçao e sua respectiva saida da funçao

---
+++ ex3.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let i = 0, len = arr.length; i < len; i += 1) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(typeof myRemoveWithoutCopy, 'function');
+
+//1 - Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1,2,4], "mensagem de erro para o exerício 1");
