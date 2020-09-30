70dd0d0d5646c9aa41eb10fa0b62b72d8ff2b076
vitorAmorims <vtamorims@gmail.com>
2020-09-30 08:31:41 -0300

    inserido testes TDD para param igual: string, number e undefined

--- subscribe_functions_tdd/ex3.js
+++ subscribe_functions_tdd/ex3.js
@@ -3,11 +3,14 @@ Use a variável parameter como parâmetro da função abaixo, escreva testes par
 */
 const assert = require('assert')
 const greaterThanTen = (array) => {
+    if (typeof(array) === 'string'  || typeof(array) === 'number' || typeof(array) === 'undefined') {
+        throw 'Por favor, passe array com elementos Number!'
+    }
     let results = [];
     for (let i = 0; i < array.length; i += 1) {
-      if (array[i] > 10) {
-        results.push(array[i]);
-      }
+        if (array[i] > 10) {
+            results.push(array[i]);
+        }
     }
     return results;
   };
@@ -17,4 +20,8 @@ const result = [32, 21];
 // console.log(greaterThanTen(parameter));
 assert.strictEqual(typeof greaterThanTen,'function');
 assert.deepStrictEqual(greaterThanTen(parameter), result);
-assert.notDeepStrictEqual(greaterThanTen(parameter, [30]))
+assert.notDeepStrictEqual(greaterThanTen(parameter, [30]));
+assert.throws(() => {
+    greaterThanTen('a');
+});
+console.log(greaterThanTen());
