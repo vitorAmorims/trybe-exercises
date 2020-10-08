d978c983aa77b2fa13cc189d9d4c3b67815dd057
vitorAmorims <vtamorims@gmail.com>
2020-10-08 13:39:43 -0300

    ex1 destructuring

---
+++ ex1Destructuring.js
@@ -0,0 +1,29 @@
+/* freeCodeCamp
+Substitua as duas atribuições por uma atribuição de desestruturação equivalente. Ele ainda deve atribuir as variáveis todaye tomorrowos valores de todaye tomorrowdo HIGH_TEMPERATURESobjeto.
+*/
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const {today} = HIGH_TEMPERATURES;
+  const {tomorrow} = HIGH_TEMPERATURES;
+  
+  // Only change code above this line
+
+
+
+
+
+
+
+
+
+
+
+
+
