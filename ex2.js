8b11df31c35133b8e10b7d148792f4c172787b7b
vitorAmorims <vtamorims@gmail.com>
2020-10-23 11:44:50 -0300

    sob o retorno da Promise, encadeando metodo then

--- ex2.js
+++ ex2.js
@@ -1,3 +1,13 @@
+function divPor23510(number) {
+    let resultEx3 = [];
+    let divisores = [2,3,5,10];
+    for (let i = 0; i < divisores.length; i++) {
+        resultEx3.push(number/divisores[i])
+    }
+    return resultEx3;
+}
+
+
 const ex2 = new Promise((resolve, reject) => {
     const numbers = [];
     for (let i = 0; i < 10; i++) {
@@ -9,13 +19,24 @@ const ex2 = new Promise((resolve, reject) => {
     const total = newNumbers.reduce((acc,element) => acc + element);
     console.log("Total dos Elementos: "+ total)
     if (total < 800) {
-        resolve("Sucesso na operação!");
+        resolve(divPor23510(total));
     } else {
-        reject("O total dos elementos supera 800!")
+        reject("O total dos elementos supera 800! Essa promise deve estar quebrada!")
     }
     return total;
 })
 
+const maiorQue200 = (param) => {
+    let result = [];
+    for (let i = 0; i < param.length; i++) {
+        if (param[i] > 200) {
+            result.push(param[i]);
+        }
+    }
+    return result;
+}
+
 ex2
-.then((value) => console.log(value))
+.then(maiorQue200)
+.then(console.log)
 .catch((err) => console.log(err))
