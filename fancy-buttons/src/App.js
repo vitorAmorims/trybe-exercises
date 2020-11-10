807fdb9373bf546bc195a7a12a1110ccae5b3a42
vitorAmorims <vtamorims@gmail.com>
2020-11-10 16:17:50 -0300

    this e bind

--- fancy-buttons/src/App.js
+++ fancy-buttons/src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import { log, log1, log2 } from './funcoes'
+import React, { Component } from "react";
+import "./App.css";
+import { log1, log2 } from "./funcoes";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.log = this.log.bind(this);
+  }
+  log() {
+    console.log(this)
+    console.log('clicou ai amigão')
+  }
   render() {
     return (
       <div className="App">
-        <button onClick={log}>Pai</button>
+        <button onClick={this.log}>Pai</button>
         <button onMouseOver={log1}>Filho</button>
         <button onDoubleClick={log2}>Espirito-Santo</button>
       </div>
