9cc25abd0f5200c3434c840503b9229914178643
vitorAmorims <vtamorims@gmail.com>
2020-11-11 09:44:25 -0300

    etapa 2 transformando funcao para classe e importando React

--- aula-ao-vivo/src/App.js
+++ aula-ao-vivo/src/App.js
@@ -1,14 +1,27 @@
+import React, { Component } from 'react';
 import './App.css';
 import data from './data';
 
-function App() {
-  return (
-    <div className="App">
-      {data.map(({color, value}) => {
-       return (<li key={value}>{color} come as {value}</li>);
-      })}
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       {data.map(({color, value}) => {
+//        return (<li key={value}>{color} come as {value}</li>);
+//       })}
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {data.map(({color, value}) => {
+          return (<li key={value}>{color} come as {value}</li>);
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
