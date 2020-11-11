aa6506a80a559cb78b3d2c2ae7d9a5bbc4047012
vitorAmorims <vtamorims@gmail.com>
2020-11-10 21:20:04 -0300

    para fixar

--- teste/src/App.js
+++ teste/src/App.js
@@ -8,30 +8,28 @@ class App extends Component {
       numeroDeCliques: 0,
       numeroDeCliques1: 0,
       numeroDeCliques2: 0
-    }
+    };
   }
   handleClick = () => {
     this.setState((estadoAnterior, _props) => ({
       numeroDeCliques: estadoAnterior.numeroDeCliques + 1
     }));
-  }
+  };
   handleClick1 = () => {
     this.setState((estadoAnterior1, _props) => ({
       numeroDeCliques1: estadoAnterior1.numeroDeCliques1 + 1
     }));
-  }
+  };
   handleClick2 = () => {
     this.setState((estadoAnterior2, _props) => ({
       numeroDeCliques2: estadoAnterior2.numeroDeCliques2 + 1
     }));
-  }
+  };
   render() {
     return (
       <div className="App">
         <h1>Aqui vou eu.... :)</h1>
-        <button onClick={this.handleClick}>
-          {this.state.numeroDeCliques}
-        </button>
+        <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
         <button onClick={this.handleClick1}>
           {this.state.numeroDeCliques1}
         </button>
