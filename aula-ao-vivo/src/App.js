78ef7caeafc8825d73762522c950c00c3685be2a
vitorAmorims <vtamorims@gmail.com>
2020-11-11 09:47:44 -0300

    Criando campo de imput

--- aula-ao-vivo/src/App.js
+++ aula-ao-vivo/src/App.js
@@ -16,8 +16,11 @@ class App extends Component {
   render() {
     return (
       <div>
+        <div>
+          <input type="text"/>
+        </div>
         {data.map(({color, value}) => {
-          return (<li key={value}>{color} come as {value}</li>);
+          return (<li key={value}>{color} coded as {value}</li>);
         })}
       </div>
     )
